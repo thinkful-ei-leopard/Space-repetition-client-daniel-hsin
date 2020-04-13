3ce64a9c2079558ddb6b4c1ee93a791475671065
Daniel Fong <daniel.fong.mv@gmail.com>
2020-04-13 13:54:54 -0600

    added total counts

--- src/components/WordsList/WordsList.js
+++ src/components/WordsList/WordsList.js
@@ -4,9 +4,27 @@ import './WordsList.css'
 
 export default class WordsList extends React.Component {
     render() {
+        function totalCorrect(arr) {
+            let count = 0;
+            for (let i = 0; i < arr.length; i++) {
+                count = count + arr[i].correct_count;
+            }
+            return count
+        }
+
+        function totalIncorrect(arr) {
+            let count = 0;
+            for (let i = 0; i < arr.length; i++) {
+                count = count + arr[i].correct_count;
+            }
+            return count
+        }
+
         return(
             <ul className='words-list-ul'>
-                {this.props.words.map(word => <Word word={word}/>)}
+                {this.props.words.map(word => <Word key={word.id} word={word}/>)}
+                <span>Total Times Correct: {totalCorrect(this.props.words)}</span>
+                <span>Total Times Incorrect: {totalIncorrect(this.props.words)}</span>
             </ul>
         )
     }
