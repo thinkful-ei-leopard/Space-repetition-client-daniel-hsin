c628d44f87aec003292fcfa903b4e1723d356ddb
Daniel Fong <daniel.fong.mv@gmail.com>
2020-04-13 13:16:53 -0600

    added word counts

--- src/components/WordsList/Word/Word.js
+++ src/components/WordsList/Word/Word.js
@@ -2,10 +2,13 @@ import React from 'react'
 
 export default class Word extends React.Component {
     render () {
+        console.log(this.props.word)
         return (
             <li>
                 <span>{this.props.word.original}</span>
                 <span>{this.props.word.translation}</span>
+                <span>{this.props.word.correct_count}</span>
+                <span>{this.props.word.incorrect_count}</span>
             </li>
         )
     }
