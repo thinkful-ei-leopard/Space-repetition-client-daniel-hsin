b386e2c4f83e471fd04f67ec0768a507457c4572
Daniel Fong <daniel.fong.mv@gmail.com>
2020-04-15 12:31:22 -0600

    changes?

--- src/services/token-service.js
+++ src/services/token-service.js
@@ -6,6 +6,7 @@ const _TEN_SECONDS_IN_MS = 10000
 
 const TokenService = {
   saveAuthToken(token) {
+    console.log(token)
     window.localStorage.setItem(config.TOKEN_KEY, token)
   },
   getAuthToken() {
