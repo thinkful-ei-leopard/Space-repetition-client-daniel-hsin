828d6a71a79e2331b7a7d9cdc7279238dc9f0b7b
Daniel Fong <daniel.fong.mv@gmail.com>
2020-04-16 15:24:17 -0600

    got test to pass

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+import { UserProvider } from '../../contexts/UserContext'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(
+  <BrowserRouter>
+    <UserProvider>
+      <App />
+    </UserProvider>
+  </BrowserRouter>
+  , div)
   ReactDOM.unmountComponentAtNode(div)
 })
